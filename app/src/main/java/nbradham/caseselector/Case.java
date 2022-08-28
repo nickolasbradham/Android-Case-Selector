9a3fd454d251bf42581e6ae266a98703e8d64b8a
nickolasbradham <nickolasbradham@gmail.com>
2022-08-27 20:03:57 -0600

    v2.0 initial release

--- app/src/main/java/nbradham/caseselector/Case.java
+++ app/src/main/java/nbradham/caseselector/Case.java
@@ -1,20 +1,41 @@
 package nbradham.caseselector;
 
+import androidx.annotation.NonNull;
+
 import java.util.Arrays;
 
 public class Case {
 
-    private final String name;
+    private final String fileName, name;
     private final String[] chars;
     private final char[] sfws;
 
-    protected Case(String caseName, String char0, String char1, char sfw0, char sfw1) {
-        name = caseName;
-        chars = new String[]{char0, char1};
+    protected Case(String fileName, String[] setChars, char sfw0, char sfw1) {
+        this.fileName = fileName;
+        name = this.fileName.substring(0, this.fileName.lastIndexOf('.'));
+        chars = setChars;
         sfws = new char[]{sfw0, sfw1};
     }
 
     protected char getSFW(int i) {
         return sfws[i];
+    }
+
+    protected String getFileName() {
+        return fileName;
+    }
+
+    protected String getName() {
+        return name;
+    }
+
+    protected String[] getChars() {
+        return chars;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return fileName + Arrays.toString(chars) + Arrays.toString(sfws);
     }
 }
--- app/src/main/java/nbradham/caseselector/MainActivity.java
+++ app/src/main/java/nbradham/caseselector/MainActivity.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ImageView;
 import android.widget.RadioButton;
-import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,17 +20,19 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Properties;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String OPT_SFW = "sfw", OPT_BOTH_SIDES = "both", OPT_NO_REPEAT = "diff", CH_SAFE = "s", CH_EROTIC = "e", CH_EXPLICIT = "x";
+    private static final Random RAND = new Random();
 
     private final Properties props = new Properties();
+    private final ArrayList<String> history = new ArrayList<>();
 
     private Case[] allCases;
     private File imgDir, hist, opts;
-    private ArrayList<String> history = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,34 +69,24 @@ public class MainActivity extends AppCompatActivity {
                 history.add(scan.nextLine());
             scan.close();
 
-            String last = history.get(history.size() - 1);
-
-            ((TextView) findViewById(R.id.caseText)).setText(last);
-            ((ImageView) findViewById(R.id.iImage)).setImageBitmap(BitmapFactory.decodeFile(Paths.get(imgDir.toString(), last + ".jpg").toString()));
+            setCaseView(parseCase(history.get(history.size() - 1)));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
 
         ArrayList<Case> tmpCases = new ArrayList<>();
-        for (String s : Objects.requireNonNull(imgDir.list())) {
-            String name = s.substring(0, s.lastIndexOf('.'));
-            String[] chars = name.substring(2).split("-");
-            tmpCases.add(new Case(name, chars[0], chars.length > 1 ? chars[1] : null, s.charAt(0), s.charAt(1)));
-        }
+        for (String s : Objects.requireNonNull(imgDir.list()))
+            tmpCases.add(parseCase(s));
         allCases = tmpCases.toArray(new Case[0]);
     }
 
     public void onRoll(View v) {
-        ArrayList<Case> avail = new ArrayList<>();
-        boolean both = Boolean.parseBoolean(props.getProperty(OPT_BOTH_SIDES));
-        byte tarSFW = getSFWByte(props.getProperty(OPT_SFW));
-        for (Case c : allCases)
-            if ((both && Math.max(getSFWByte(c.getSFW(0)), getSFWByte(c.getSFW(1))) < tarSFW) || (!both && Math.max(getSFWByte(c.getSFW(0)), getSFWByte(c.getSFW(1))) < tarSFW))
-                avail.add(c);
+        handleRoll();
     }
 
     public void onReroll(View v) {
-        //TODO Write function.
+        history.remove(history.size() - 1);
+        handleRoll();
     }
 
     public void onOption(View v) throws IOException {
@@ -122,5 +113,78 @@ public class MainActivity extends AppCompatActivity {
 
     private byte getSFWByte(char c) {
         return getSFWByte(c + "");
+    }
+
+    private byte getSFWByte(Case c, int i) {
+        return getSFWByte(c.getSFW(i));
+    }
+
+    private Case parseCase(String fileName) {
+        String[] chars = fileName.substring(2, fileName.lastIndexOf('.')).split("-");
+        for (byte n = 0; n < chars.length; n++)
+            chars[n] = chars[n].replaceAll("[0-9]*$", "");
+
+        return new Case(fileName, chars, fileName.charAt(0), fileName.charAt(1));
+    }
+
+    private boolean lastHasDifChars(Case c) {
+        if (history.size() > 0)
+            for (String ch : parseCase(history.get(history.size() - 1)).getChars())
+                for (String ct : c.getChars())
+                    if (ch.equals(ct))
+                        return false;
+        return true;
+    }
+
+    private void setCaseView(Case c) {
+        ((TextView) findViewById(R.id.caseText)).setText(c.getName());
+        ((ImageView) findViewById(R.id.iImage)).setImageBitmap(BitmapFactory.decodeFile(Paths.get(imgDir.toString(), c.getFileName()).toString()));
+    }
+
+    private void handleRoll() {
+        Case[] avail = getAvailableCases();
+
+        System.out.println("Pool: " + Arrays.toString(avail));
+
+        if (avail.length <= 0) {
+            String last = history.get(history.size()-1);
+            history.clear();
+            history.add(last);
+
+            avail = getAvailableCases();
+            System.out.println("Reset Pool: " + Arrays.toString(avail));
+        }
+
+        Case sel = avail[RAND.nextInt(avail.length)];
+        setCaseView(sel);
+        history.add(sel.getFileName());
+
+        try {
+            FileWriter fw = new FileWriter(hist);
+            history.forEach(s -> {
+                try {
+                    fw.append(s).append('\n');
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Case[] getAvailableCases() {
+        ArrayList<Case> avail = new ArrayList<>();
+        boolean both = Boolean.parseBoolean(props.getProperty(OPT_BOTH_SIDES)), noRepeat = Boolean.parseBoolean(props.getProperty(OPT_NO_REPEAT));
+        byte tarSFW = getSFWByte(props.getProperty(OPT_SFW));
+
+        for (Case c : allCases) {
+            byte sfw0 = getSFWByte(c, 0), sfw1 = getSFWByte(c, 1);
+            if ((both && Math.max(sfw0, sfw1) <= tarSFW || !both && Math.min(sfw0, sfw1) <= tarSFW) && !history.contains(c.getFileName()) && (!noRepeat || lastHasDifChars(c)))
+                avail.add(c);
+        }
+
+        return avail.toArray(new Case[0]);
     }
 }
